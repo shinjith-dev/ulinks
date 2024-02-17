25a31072a65cefe1e1033cf2f7dc7594e277aa83
shinjith-dev <shinjithpallikkara@gmail.com>
2024-02-17 15:43:40 +0530

    feat: Add md files for links

---
+++ app/collection/layout.tsx
@@ -0,0 +1,9 @@
+import "@/styles/markdown.css"
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <div className="markdown">{children}</div>;
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/* <Navbar /> */}
+
+        <main className="app">{children}</main>
+      </body>
     </html>
   );
 }
